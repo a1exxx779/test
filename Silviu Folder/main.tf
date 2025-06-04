4c84ec42f2a115dfe48601fd98624c5b83400685
MadSilviu <silviuconstantin.stroe@yahoo.com>
2025-06-04 12:55:39 +0000

    j

--- Silviu Folder/main.tf
+++ Silviu Folder/main.tf
@@ -1,4 +1,7 @@
 resource "azurerm_resource_group" "claudiu" {
   name     = "Alex-juneploy-test"
   location = "West Europe"
+  tags = {
+    owner = "silviu.stroe@redbull.com"
+  }
 }
--- Silviu Folder/providers.tf
+++ Silviu Folder/providers.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
 provider "azurerm" {
     resource_provider_registrations = "none"
     features {}
